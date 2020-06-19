ad70ce0fa1f7c66770b6a9c5b7f4f9506ec7ef10
Patrick Kimes <patrick.kimes@gmail.com>
2020-06-19 18:46:53 -0400

    Initial carryover from 2019

---
+++ code_of_conduct.md
@@ -0,0 +1,49 @@
+# Code of Conduct
+
+We (the organizers and facilitators of the 2019 Replicathon at UPR-RP) are committed to providing a safe, inclusive, welcoming, and harassment-free experience for everyone. This Code of Conduct is based on the [rOpenSci Code of Conduct](https://ropensci.org/code-of-conduct/).
+
+We try to cultivate a community with shared values, where people are comfortable exploring ideas, asking questions, and saying things like "I don't understand" or "Why". Assume competence in the people you interact with. There are no stupid questions.
+
+Be considerate in speech and actions, and actively seek to acknowledge and respect the boundaries of fellow community members.
+
+Take care of each other. Alert one of the organizers or facilitators if you notice a dangerous situation, someone in distress, or a potential violation of this Code of Conduct, even if it seems inconsequential.
+
+We do not tolerate harassment in any form. Refrain from demeaning, discriminatory, or harassing behavior and speech. Harassment includes, but is not limited to:
+
+- offensive comments related to gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, religion (or lack thereof), or technology choices
+- unwelcome comments regarding a person's lifestyle choices and practices, including those related to food, health, parenting, drugs, or employment
+- sustained or willful disruption of discussions, talks, or other events
+- deliberate intimidation
+- continued one-on-one communication after requests to cease
+- unwanted photography or recording, including logging online activity for harassment purposes
+- pattern of inappropriate social contact, such as requesting/assuming inappropriate levels of intimacy with others
+- inappropriate physical contact without consent or after a request to stop
+- stalking or following
+- threats of violence or incitement of violence towards any individual, including encouraging a person to commit suicide or to engage in self-harm
+- use of sexual or discriminatory imagery, comments, or jokes
+- unwelcome sexual attention
+- deliberate 'outing' of any aspect of a person's identity without their consent except as necessary to protect vulnerable people from intentional abuse
+- deliberate misgendering or use of 'dead' or rejected names
+- publication of non-harassing private communication
+
+We prioritize marginalized people's safety over privileged people's comfort. We will not act on complaints regarding:
+
+- 'reverse' -isms, including 'reverse racism,' 'reverse sexism,' and 'cisphobia'
+- reasonable communication of boundaries, such as "leave me alone," "go away," or "I'm not discussing this with you"
+- communicating in a 'tone' you don't find congenial
+- criticizing racist, sexist, cissexist, or otherwise oppressive behavior or assumptions
+
+If anyone engages in harassing behavior, the Committee will take action that may include warning the offender or asking them to leave an event or an online channel either temporarily or permanently, or seeking help from law enforcement.
+
+This Code of Conduct applies to all people participating in the rOpenSci community, including rOpenSci staff and leadership. It applies to all modes of interaction online including GitHub project repositories, the rOpenSci discussion forum, Slack, Community Calls, and in person at rOpenSci-hosted events or events officially endorsed by rOpenSci, including social gatherings affiliated with the event.
+
+## Reporting Guidelines
+
+If you experience or witness unacceptable behavior, or have any other concerns, please contact one of the facilitators as soon as possible. If any facilitator has a conflict of interest with a report, they will be recused and will not have access to the content or process of the report followup.
+
+## License and Credits
+
+This Code of Conduct is adapted from the public domain [rOpenSci Code of Conduct](https://ropensci.org/code-of-conduct/), which was in turn influenced by and adapted from many sources including the [Open Source and Feelings](https://youtu.be/nizfHxg8y3o) talk by [Audrey Eschright](http://lifeofaudrey.com/), the R Consortium R Community Diversity and Inclusion Working Group's draft [Code of Conduct](https://github.com/RConsortium/RCDI-WG/blob/0ca0a91dccc9296ff53a5806f52a2a49dbb8850d/conduct/code-of-conduct.md), the [Geek Feminism anti-harassment policy](http://geekfeminism.wikia.com/wiki/Community_anti-harassment/Policy), and the rOpenSci Community Call: [How do I create a Code of Conduct for my event/lab/codebase?](https://ropensci.org/commcalls/2016-12-15/)
+
+To the extent possible under law, we have waived all copyright and related or neighboring rights to this Code of Conduct.
+
---
+++ downloadData.R
@@ -0,0 +1,219 @@
+#' Download, subset, and tidy pharmacogenomics datasets 
+#'
+#' (c) 2017 Alejandro Reyes, Keegan Korthauer
+#' (c) 2019 Patrick Kimes, Kelly Street
+
+library(Biobase)
+library(PharmacoGx)
+library(tidyverse)
+
+## download parsed data for GDSC, CCLE studies from Haibe-Kains lab
+GDSC <- downloadPSet("GDSC")
+CCLE <- downloadPSet("CCLE")
+
+## only use intersection of two studies
+common <- intersectPSet(list('CCLE' = CCLE, 'GDSC' = GDSC),
+                        intersectOn = c("cell.lines", "drugs"),
+                        strictIntersect = TRUE)
+
+## specify output directory
+dir.create("data", showWarnings = FALSE)
+
+## ##############################################################################
+## Parse raw dose and viability measures merged by Haibe-Kains et al. (2014)
+## ##############################################################################
+
+## helper function to transform 'raw' data in PSet to data.frame 
+rawToDataFrame <- function(dataset) {
+    rawdat <- common[[dataset]]@sensitivity$raw
+    names(dimnames(rawdat)) <- c("cellLine_drug", "doseID", "measure")
+    rawdat <- as.data.frame.table(rawdat, responseName = "value",
+                                  stringsAsFactors = FALSE)
+    rawdat <- dplyr::mutate(rawdat, value = as.numeric(as.character(value)))
+    rawdat <- tidyr::spread(rawdat, measure, value)
+    rawdat <- tidyr::separate(rawdat, cellLine_drug,
+                              c("cellLine", "drug"), sep = "_")
+    rawdat <- dplyr::rename(rawdat, concentration = Dose, viability = Viability)
+    rawdat <- dplyr::filter(rawdat, !is.na(concentration), !is.na(viability))
+    rawdat
+}
+
+## transform PSets to data.frame (long)
+rawData <- dplyr::bind_rows(CCLE = rawToDataFrame("CCLE"),
+                            GDSC = rawToDataFrame("GDSC"),
+                            .id = "study")
+
+## reorder columns
+colOrder <- c("cellLine", "drug", "doseID", "concentration", "viability", "study")
+rawData <- rawData[, colOrder]
+
+## determine set of cell lines in our data subset
+allCellLines <- unique(rawData$cellLine)
+
+## save as RDS file
+saveRDS(rawData, file = file.path("data", "rawPharmacoData.rds"))
+
+## ## alernatively, save as csv files
+## write.table(rawData, sep = ",", quote = FALSE, col.names = TRUE,
+##             row.names = FALSE, file = file.path("data", "rawPharmacoData.csv"))
+
+
+## ##############################################################################
+## Parse summarized sensitivity values from Haibe-Kains et al. (2014)
+## -- will use "published" values (data also includes recomputed values)
+## ##############################################################################
+
+## helper function to transform summarized 'profile' data in PSet to data.frame 
+profilesToDataFrame <- function(dataset) {
+    sumdat <- common[[dataset]]@sensitivity$profiles
+    keepCols <- c("ic50_published", "auc_published")
+    sumdat <- sumdat[, keepCols]
+    sumdat <- tibble::rownames_to_column(sumdat, "cellLine_drug")
+    sumdat <- dplyr::rename_if(sumdat, is.numeric, ~gsub("_published", "", .))
+    sumdat <- tidyr::separate(sumdat, cellLine_drug,
+                              c("cellLine", "drug"), sep = "_")
+    sumdat
+}
+
+## transform PSets to data.frame (wide)
+sumData <- dplyr::inner_join(profilesToDataFrame("CCLE"),
+                             profilesToDataFrame("GDSC"),
+                             by = c("cellLine", "drug"),
+                             suffix = c("_CCLE", "_GDSC"))
+
+## save as RDS file
+saveRDS(sumData, file = file.path("data", "summarizedPharmacoData.rds"))
+
+## ## alernatively, save as csv files
+## write.table(sumData, sep = ",", quote = FALSE, col.names = TRUE,
+##             row.names = FALSE, file = file.path("data", "summarizedPharmacoData.csv"))
+
+
+## ##############################################################################
+## Parse gene expression measurements merged by Haibe-Kains et al. (2014)
+## ##############################################################################
+
+## to parse gene expression data set to TRUE
+if (FALSE) { 
+    ## helper function to transform gene expression data in PSet to data.frame 
+    exprToDataFrame <- function(dataset) {
+        rna_pdat <- pData(common[[dataset]]@molecularProfiles$rna)
+        rna_vals <- exprs(common[[dataset]]@molecularProfiles$rna)
+        ## only keep first instance of each cell line
+        rna_keep <- !duplicated(rna_pdat$cellid)
+        
+        rna_vals <- rna_vals[, rna_keep]
+        colnames(rna_vals) <- rna_pdat$cellid[rna_keep]
+        
+        rna_vals <- as.data.frame(rna_vals, optional = TRUE)
+        rna_vals <- tibble::rownames_to_column(rna_vals, "gene")
+        rna_vals <- tidyr::gather(rna_vals, cellLine, expression, -gene)
+        rna_vals
+    }
+
+    ## transofmr PSets to data.frame (wide)
+    exprData <- dplyr::inner_join(exprToDataFrame("CCLE"),
+                                  exprToDataFrame("GDSC"),
+                                  by = c("gene", "cellLine"),
+                                  suffix = c("_CCLE", "_GDSC"))
+
+    ## note: expression data includes more cell lines than the intersection
+    ## subset on cell lines with sensitivity data
+    exprData <- dplyr::filter(exprData, cellLine %in% !! allCellLines)
+
+    ## save as RDS file
+    saveRDS(exprData, file = file.path("data", "exprPharmacoData.rds"))
+    
+    ## ## alernatively, save as csv files
+    ## write.table(exprData, sep = ",", quote = FALSE, col.names = TRUE,
+    ##             row.names = FALSE, file = file.path("data", "exprPharmacoData.csv"))
+}
+
+
+## ##############################################################################
+## Parse cell line metadata cleaned by Haibe-Kains et al. (2014)
+## -- parse cell line metadata in CCLE, GDSC objects separately
+## -- overlap is moderate, will keep data separate and only use subset of columns
+## ##############################################################################
+
+## to parse cell line metadata set to TRUE
+if (FALSE) { 
+    ## parse cell line metadata from CCLE
+    clDataCCLE <- common[["CCLE"]]@cell
+    clDataCCLE <- dplyr::select(clDataCCLE, cellid, tissueid, Gender,
+                                Site.Primary, Histology, Hist.Subtype1)
+    clDataCCLE <- dplyr::filter(clDataCCLE, cellid %in% !! allCellLines)
+    clDataCCLE <- dplyr::rename(clDataCCLE,
+                                cellLine = cellid,
+                                tissue = tissueid,
+                                gender = Gender, 
+                                primarySite = Site.Primary,
+                                histology = Histology,
+                                subtype = Hist.Subtype1)
+
+    ## parse cell line metadata from GDSC
+    clDataGDSC <- common[["GDSC"]]@cell
+    clDataGDSC <- dplyr::select(clDataGDSC, cellid, tissueid, Primary.site,
+                                Primary.histology, Histology.subtype)
+    clDataGDSC <- dplyr::filter(clDataGDSC, cellid %in% !! allCellLines)
+    clDataGDSC <- dplyr::rename(clDataGDSC,
+                                cellLine = cellid,
+                                tissue = tissueid, 
+                                primarySite = Primary.site,
+                                histology = Primary.histology,
+                                subtype = Histology.subtype)
+
+    ## save as RDS file
+    saveRDS(clDataCCLE, file = file.path("data", "cellLineDataCCLE.rds"))
+    saveRDS(clDataGDSC, file = file.path("data", "cellLineDataGDSC.rds"))
+
+    ## ## alernatively, save as csv files
+    ## write.table(clDataCCLE, sep = ",", quote = FALSE, col.names = TRUE,
+    ##             row.names = FALSE, file = file.path("data", "cellLineDataCCLE.csv"))
+    ## write.table(clDataGDSC, sep = ",", quote = FALSE, col.names = TRUE,
+    ##             row.names = FALSE, file = file.path("data", "cellLineDataGDSC.csv"))
+}
+
+
+## ##############################################################################
+## Parse subset of drug metadata cleaned by Haibe-Kains et al. (2014)
+## -- parse cell line metadata in CCLE, GDSC objects separately
+## ##############################################################################
+
+## to parse drug metadata set to TRUE
+if (FALSE) { 
+    ## parse drug metadata from CCLE
+    drugMapCCLE <- common[["CCLE"]]@curation$drug
+    drugMapCCLE <- dplyr::mutate_if(drugMapCCLE, is.factor, as.character)
+    drugDataCCLE <- common[["CCLE"]]@drug
+    drugDataCCLE <- dplyr::left_join(drugDataCCLE, drugMapCCLE, by = c("drug.name" = "CCLE.drugid"))
+    drugDataCCLE <- dplyr::select(drugDataCCLE,
+                                  drug = unique.drugid,
+                                  class = Class)
+
+    ## parse drug metadata from GDSC
+    drugMapGDSC <- common[["GDSC"]]@curation$drug
+    drugMapGDSC <- dplyr::mutate_if(drugMapGDSC, is.factor, as.character)
+    drugDataGDSC <- common[["GDSC"]]@drug
+    drugDataGDSC <- dplyr::left_join(drugDataGDSC, drugMapGDSC, by = c("drug.name" = "GDSC.drugid"))
+    drugDataGDSC <- dplyr::select(drugDataGDSC,
+                                  drug = unique.drugid,
+                                  type = Drug.type,
+                                  targetted = Drug.class.II)
+
+    ## merge drug data
+    drugData <- dplyr::full_join(drugDataGDSC, drugDataCCLE, by = "drug")
+
+    ## save as RDS file
+    saveRDS(drugData, file = file.path("data", "drugData.rds"))
+
+    ## ## alernatively, save as csv files
+    ## write.table(drugData, sep = ",", quote = FALSE, col.names = TRUE,
+    ##             row.names = FALSE, file = file.path("data", "drugData.csv"))
+}
+
+
+
+
+
+
